d62a3810052a4b6944594f5099b6c68ccecd0622
Cemil <chrisw@cydeo.com>
2022-08-30 11:31:31 +0300

    initial commit

---
+++ src/main/java/com/cydeo/entity/Parent.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Parent extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Parent(int id, String firstName, String lastName, String phoneNumber) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ src/main/java/com/cydeo/entity/Teacher.java
@@ -0,0 +1,28 @@
+package com.cydeo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Teacher extends BaseEntity {
+
+    private String firstName;
+    private String lastName;
+
+    private String phoneNumber;
+    private BigDecimal salary;
+
+    public Teacher(int id, String firstName, String lastName, String phoneNumber, BigDecimal salary) {
+        super(id);
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.salary = salary;
+    }
+
+}
